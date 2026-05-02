7b0db83874228e530de0977d0856db9881267b3a
Corbin Riches <corbin@corbinriches.com>
2026-05-02 16:49:22 -0500

    Polish local guestbook widget

---
+++ src/data/kindnessMap.js
@@ -0,0 +1,82 @@
+export const kindnessMap = {
+	// 1. harmless test words
+	testword: 'stardust',
+	testwords: 'stardust',
+	testwording: 'stardusting',
+	pineapple: 'friend',
+	pineapples: 'friends',
+	grumpy: 'sleepy',
+	'very grumpy': 'very sleepy',
+
+	// 2. mild profanity
+	damn: 'dang',
+	dammit: 'dang it',
+	damned: 'dang',
+	hell: 'heck',
+	crap: 'compost',
+	crappy: 'composty',
+	sucks: 'is rough',
+
+	// 3. stronger profanity
+	shit: 'beans',
+	shits: 'beans',
+	shitty: 'bean-filled',
+	sh1t: 'beans',
+	'sh*t': 'beans',
+	fuck: 'heck',
+	fucks: 'hecks',
+	fucked: 'hecked',
+	fucking: 'very',
+	'f*ck': 'heck',
+	'f**k': 'heck',
+	fuk: 'heck',
+	bullshit: 'static',
+	'bull sh*t': 'static',
+	wtf: 'oh dear',
+
+	// 4. insults / name-calling
+	idiot: 'rascal',
+	idiots: 'rascals',
+	moron: 'neighbor',
+	morons: 'neighbors',
+	stupid: 'silly goose',
+	dumb: 'kindly',
+	loser: 'friend',
+	losers: 'friends',
+	jerk: 'rascal',
+	jerks: 'rascals',
+	asshole: 'neighbor',
+	assholes: 'neighbors',
+	'a-hole': 'neighbor',
+	bastard: 'rascal',
+	bitch: 'neighbor',
+	bitches: 'neighbors',
+
+	// 5. crude/gross terms
+	poop: 'compost',
+	poopy: 'composty',
+	piss: 'static',
+	pissed: 'static',
+	gross: 'compost',
+	nasty: 'dusty',
+	dick: 'rascal',
+	dicks: 'rascals',
+
+	// 6. spam/gibberish
+	asdf: 'static',
+	asdfasdf: 'static',
+	qwerty: 'static',
+	'buy now': 'static',
+	'click here': 'static',
+	'free money': 'static',
+	'visit my site': 'static',
+
+	// 7. hateful/slur terms placeholder section
+	// Keep severe private additions neutral, not funny.
+	'hate you': '[not welcome]',
+	'go away': 'neighbor',
+	'shut up': 'static',
+	bigot: '[not welcome]',
+	racist: '[not welcome]',
+	sexist: '[not welcome]',
+};
